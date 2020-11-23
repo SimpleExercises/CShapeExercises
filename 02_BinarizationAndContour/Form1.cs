f94511f8c2d0210f4a6bf00b7f12004370a1cff9
NUZEROVI <nuvmcdxliv@gmail.com>
2020-11-23 23:10:02 +0800

    Inital 02_BinarizationAndContour

---
+++ 02_BinarizationAndContour/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_BinarizationAndContour
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+                f.bmp2RGB(bmp);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void grayBaseOnGreenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = f.grayImg(f.greenArr);
+        }
+    }
+}
